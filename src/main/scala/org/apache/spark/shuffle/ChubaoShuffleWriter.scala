ee7182fb04231666f1b9973b5cca487797f8c478
Justice <justice_103@126.com>
2020-02-26 20:51:42 +0800

    add chubao sorter

--- src/main/scala/org/apache/spark/shuffle/ChubaoShuffleWriter.scala
+++ src/main/scala/org/apache/spark/shuffle/ChubaoShuffleWriter.scala
@@ -29,8 +29,35 @@ private[spark] class ChubaoShuffleWriter[K, V, C] (
   extends ShuffleWriter[K, V]
   with Logging {
 
+  private val dep = handle.dependency
+
   override def write(records: Iterator[Product2[K, V]]): Unit = {
-    //TODO:
+    sorter = if (dep.mapSideCombine) {
+      require(dep.aggregator.isDefined, "Map-side combine without Aggregator specified!")
+      new ChubaoSorter[K, V, C](
+        context, dep.aggregator, Some(dep.partitioner), dep.keyOrdering, dep.serializer)
+    } else {
+      new ChubaoSorter[K, V, V](
+        context, aggregator = None, Some(dep.partitioner), ordering = None, dep.serializer)
+    }
+    sorter.insertAll(records)
+
+    // Don't bother including the time to open the merged output file in the shuffle write time,
+    // because it just opens a single file, so is typically too fast to measure accurately
+    // (see SPARK-3570).
+    val output = shuffleBlockResolver.getDataFile(dep.shuffleId, mapId)
+    val tmp = Utils.tempFileWith(output)
+    try {
+      val blockId = ShuffleBlockId(dep.shuffleId, mapId, IndexShuffleBlockResolver.NOOP_REDUCE_ID)
+      val partitionLengths = sorter.writePartitionedFile(blockId, tmp)
+      shuffleBlockResolver.writeIndexFileAndCommit(dep.shuffleId, mapId, partitionLengths, tmp)
+      mapStatus = MapStatus(blockManager.shuffleServerId, partitionLengths)
+    } finally {
+      if (tmp.exists() && !tmp.delete()) {
+        logError(s"Error while deleting temp file ${tmp.getAbsolutePath}")
+      }
+    }
+
   }
 
   /** @inheritdoc */
